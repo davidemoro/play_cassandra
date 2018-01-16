6453d45154ad94ebda5cc7bbf4f830bd4a5cdb16
Davide Moro <dmoro@tierratelematics.com>
2018-01-16 11:23:14 +0100

    add more tests

--- tests/test_play_cassandra.py
+++ tests/test_play_cassandra.py
@@ -78,3 +78,27 @@ def test_provider_condition_true(play_json):
             assert connect.assert_called_once_with(command['keyspace']) is None
             execute = connect.return_value.__enter__.return_value.execute
             assert execute.assert_called_once_with(command['query']) is None
+
+
+def test_provider_condition_false(play_json):
+    from play_cassandra import providers
+    import mock
+    provider = providers.CassandraProvider(play_json)
+    assert provider.engine is play_json
+    command = {
+        'provider': 'play_cassandra',
+        'type': 'execute',
+        'connection': {
+            'auth_provider': {
+                'username': 'user',
+                'password': 'pwd'
+                }
+            },
+        'keyspace': 'users',
+        'query': 'SELECT * from users;',
+        'condition': '1 == 0'
+        }
+    with mock.patch(
+            'play_cassandra.providers.Cluster') as cluster:
+        provider.command_execute(command)
+        assert cluster.called is False
