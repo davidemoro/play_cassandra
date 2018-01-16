b2f75bc517e622665fc124261e5ec500de0de312
Davide Moro <dmoro@tierratelematics.com>
2018-01-16 11:38:02 +0100

    more tests

--- tests/test_play_cassandra.py
+++ tests/test_play_cassandra.py
@@ -174,3 +174,83 @@ def test_provider_assertion_false(play_json):
             assert connect.assert_called_once_with(command['keyspace']) is None
             execute = connect.return_value.__enter__.return_value.execute
             assert execute.assert_called_once_with(command['query']) is None
+
+
+def test_provider_variable(play_json):
+    from play_cassandra import providers
+    import mock
+    provider = providers.CassandraProvider(play_json)
+    assert provider.engine is play_json
+    command = {
+        'provider': 'play_cassandra',
+        'type': 'execute',
+        'connection': {
+            'auth_provider': {
+                'username': 'user',
+                'password': 'pwd'
+                }
+            },
+        'variable': 'user',
+        'variable_expression': 'results[0]',
+        'keyspace': 'users',
+        'query': 'SELECT * from users WHERE userid=1;',
+        }
+    with mock.patch(
+            'play_cassandra.providers.Cluster') as cluster:
+        with mock.patch(
+                'play_cassandra.providers.auth') as auth:
+            assert 'user' not in play_json.variables
+            provider.command_execute(command)
+            assert cluster.assert_called_once_with(
+                auth_provider=auth.PlainTextAuthProvider.return_value) is None
+            assert auth.PlainTextAuthProvider.assert_called_once_with(
+                username=command['connection']['auth_provider']['username'],
+                password=command['connection']['auth_provider']['password']
+            ) is None
+            connect = cluster.return_value.__enter__.return_value.connect
+            assert connect.assert_called_once_with(command['keyspace']) is None
+            execute = connect.return_value.__enter__.return_value.execute
+            assert execute.assert_called_once_with(command['query']) is None
+            assert 'user' in play_json.variables
+            assert play_json.variables['user'] is execute.return_value[0]
+
+
+def test_provider_variable_assertion_false(play_json):
+    from play_cassandra import providers
+    import mock
+    provider = providers.CassandraProvider(play_json)
+    assert provider.engine is play_json
+    command = {
+        'provider': 'play_cassandra',
+        'type': 'execute',
+        'connection': {
+            'auth_provider': {
+                'username': 'user',
+                'password': 'pwd'
+                }
+            },
+        'variable': 'user',
+        'variable_expression': 'results[0]',
+        'keyspace': 'users',
+        'query': 'SELECT * from users WHERE userid=1;',
+        'assertion': 'variables["user"].userid == 1'
+        }
+    with mock.patch(
+            'play_cassandra.providers.Cluster') as cluster:
+        with mock.patch(
+                'play_cassandra.providers.auth') as auth:
+            assert 'user' not in play_json.variables
+            with pytest.raises(AssertionError):
+                provider.command_execute(command)
+            assert cluster.assert_called_once_with(
+                auth_provider=auth.PlainTextAuthProvider.return_value) is None
+            assert auth.PlainTextAuthProvider.assert_called_once_with(
+                username=command['connection']['auth_provider']['username'],
+                password=command['connection']['auth_provider']['password']
+            ) is None
+            connect = cluster.return_value.__enter__.return_value.connect
+            assert connect.assert_called_once_with(command['keyspace']) is None
+            execute = connect.return_value.__enter__.return_value.execute
+            assert execute.assert_called_once_with(command['query']) is None
+            assert 'user' in play_json.variables
+            assert play_json.variables['user'] is execute.return_value[0]
